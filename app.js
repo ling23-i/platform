a2a5f48cb64a2acd9d6e190a5d485234bb42bd04
ling23-i <wangxling23@qq.com>
2024-12-18 15:44:50 +0800

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const GITHUB_API_URL = 'https://api.github.com/repos/ling23-i/platform/issues';
-const GITHUB_TOKEN = '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'; // 替换为您的 GitHub Token
-
 document.addEventListener('DOMContentLoaded', () => {
     let blockchains = loadBlockchainsFromLocalStorage();
 
